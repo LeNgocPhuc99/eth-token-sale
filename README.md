2ad88a3af30e70c3c14f6f633c4fe35996547bcf
LeNgocPhuc99 <lphuc5461@gmail.com>
2022-01-01 15:36:22 +0700

    init commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# react-webpack-web3-setup
---
+++ src/js/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Web3 from 'web3';
+
+function App() {
+    return (
+        <div>
+            <h1>Hello World!!!</h1>
+        </div>
+    )
+}
+
+
+export default App;
